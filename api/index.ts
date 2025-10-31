43ad4e84890c20c234f2ae611cadd406a5045ba0
Ammar Shoeb <ammaryahia23@gmail.com>
2025-10-31 18:31:50 +0300

    Update Vercel config for static frontend deployment

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,28 @@
-import { Request, Response } from 'express';
-import { registerRoutes } from '../server/routes';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import { registerRoutes } from '../server/routes';
 
 const app = express();
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// CORS for Vercel
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // Register routes
 registerRoutes(app);
 
-// Export as Vercel serverless function
 export default app;
